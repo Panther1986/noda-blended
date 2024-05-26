72bde382569efbdf4c8a855664b2a00ffa720e6d
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-05-26 11:27:41 +0200

    tasc2

--- src/scripts/createProducts.js
+++ src/scripts/createProducts.js
@@ -3,14 +3,18 @@ import fs from "node:fs/promises";
 import { createFakeProduct } from "../utils/createFakeProduct.js";
 
 export const createProducts = async (amount) => {
-  const allProductsJson = await fs.readFile(dbPath, "utf-8");
-  const allProducts = JSON.parse(allProductsJson);
+  try {
+    const allProductsJson = await fs.readFile(dbPath, "utf-8");
+    const allProducts = JSON.parse(allProductsJson);
 
-  for (let i = 0; i < amount; i += 1) {
-    const newProduct = createFakeProduct();
-    allProducts.push(newProduct);
+    for (let i = 0; i < amount; i += 1) {
+      const newProduct = createFakeProduct();
+      allProducts.push(newProduct);
+    }
+    await fs.writeFile(dbPath, JSON.stringify(allProducts, null, 2));
+  } catch (error) {
+    console.log(error);
   }
-  await fs.writeFile(dbPath, JSON.stringify(allProducts, null, 2));
 };
 
 createProducts(5);
--- src/scripts/getExpensiveProducts.js
+++ src/scripts/getExpensiveProducts.js
@@ -3,11 +3,15 @@ import fs from "node:fs/promises";
 
 export const getExpensiveProducts = async () => {
   const PRICE = 500;
-  const allProductsJson = await fs.readFile(dbPath, "utf-8");
-  const allProducts = JSON.parse(allProductsJson);
-  const expensiveProducts = allProducts.filter(
-    (product) => product.price > PRICE
-  );
-  console.table(expensiveProducts);
+  try {
+    const allProductsJson = await fs.readFile(dbPath, "utf-8");
+    const allProducts = JSON.parse(allProductsJson);
+    const expensiveProducts = allProducts.filter(
+      (product) => product.price > PRICE
+    );
+    console.table(expensiveProducts);
+  } catch (error) {
+    console.log(error);
+  }
 };
 getExpensiveProducts();
