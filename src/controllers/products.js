ac25ba4952943aa9f182c101424e3d3f0d4b35d4
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-06-09 11:42:09 +0200

    update

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,4 +1,6 @@
-import { getAllProducts } from "../services/products.js"
+import { getAllProducts,createProduct, deleteProduct } from "../services/products.js"
+import createHttpError from 'http-errors';
+
 
 export const getProductsController = async (req, res, next) => {
   const products = await getAllProducts();
@@ -6,4 +8,25 @@ export const getProductsController = async (req, res, next) => {
   res.json({
     data: products,
   })
+};
+export const createPoductController = async (req, res) => {
+  const product = await createProduct(req.body);
+  res.status(201).json(
+    {
+      data: product,
+    }
+  )
+};
+export const deleteProductController = async (req, res,next) => {
+  const { productId } = req.params;
+  const product = await deleteProduct(productId);
+  if (!product) {
+    next(createHttpError(404, 'not found'));
+    return;
+  }
+
+  res.status(204).send();
 }
+
+
+
--- src/services/products.js
+++ src/services/products.js
@@ -2,3 +2,7 @@ import { Product } from "../db/Product.js"
 
 
 export const getAllProducts = () => Product.find();
+
+export const createProduct = (productData) => Product.create(productData);
+
+export const deleteProduct = (productId) => Product.findByIdAndDelete(productId);
